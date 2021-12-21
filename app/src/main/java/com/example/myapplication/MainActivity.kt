9799f0659dd08ded4dd12a2070c8850a64995b52
keneshbek <kenesh.sh@gmail.com>
2021-12-21 13:02:20 +0600

    я

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.`open_page.xml`)
+
+        val button = findViewById<Button>(R.id.buttonNachat)
+        button.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
     }
 }
